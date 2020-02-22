679f784b85d7ffc3c9eeac3324ea13b2c508ef43
quynhvi <quynh.nguyen@mservice.com.vn>
2020-02-22 23:51:20 +0700

    init package

--- src/main/java/com/example/jwt/JwtApplication.java
+++ src/main/java/com/example/jwt/JwtApplication.java
@@ -2,9 +2,11 @@ package com.example.jwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class JwtApplication {
+public class JwtApplication implements WebMvcConfigurer {
+	private final long MAX_AGE_SECS = 3600;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JwtApplication.class, args);
